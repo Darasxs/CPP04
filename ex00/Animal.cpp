65ef3b7d29ae8c0caba4b261d904a9718545b8d1
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2025-02-24 17:35:55 +0100

    few more functions created for ex00

--- ex00/Animal.cpp
+++ ex00/Animal.cpp
@@ -6,18 +6,18 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 15:26:52 by dpaluszk          #+#    #+#             */
-/*   Updated: 2025/02/24 16:42:59 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2025/02/24 17:13:31 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 
-Animal::Animal(void): type("Animal")
+Animal::Animal(void) : type("Animal")
 {
 	std::cout << "Default constructor for Animal called." << std::endl;
 }
 
-Animal::Animal(std::string type): type(type)
+Animal::Animal(std::string type) : type(type)
 {
 	std::cout << "Named constructor for Animal called." << std::endl;
 }
@@ -30,7 +30,22 @@ Animal::Animal(const Animal &animal)
 
 Animal &Animal::operator=(const Animal &animal)
 {
-	if(this != &animal)
+	if (this != &animal)
 		this->type = animal.type;
-	std::cout << 
+	std::cout << "Copy assignment operator for Animal called." << std::endl;
+}
+
+Animal::~Animal(void)
+{
+	std::cout << "Destructor for Animal called." << std::endl;
+}
+
+std::string Animal::getType(void) const
+{
+	return (this->type);
+}
+
+void Animal::makeSound(void) const
+{
+	std::cout << "Animal sound." << std::endl;
 }
--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -10,8 +10,8 @@ class Animal
 	Animal(std::string type);
 	Animal(const Animal &animal);
 	Animal &operator=(const Animal &animal);
-	virtual ~Animal();
-	std::string getType() const;
+	virtual ~Animal(void);
+	std::string getType(void) const;
 	virtual void makeSound(void) const;
 
   protected:
