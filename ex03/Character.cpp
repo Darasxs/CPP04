69b1be1a9de63101c9eb337a9920e551be1f0093
daras <daras@kali>
2025-03-07 09:56:15 +0100

    character class with its functions created

---
+++ ex03/Character.cpp
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daras <daras@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 09:44:36 by daras             #+#    #+#             */
+/*   Updated: 2025/03/07 09:56:02 by daras            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Character.hpp"
+
+Character::Character(void) : name("default")
+{
+    for (int i = 0; i < 4; i++)
+        inventory[i] = NULL;
+}
+
+Character::Character(std::string &name) : name(name)
+{
+    for (int i = 0; i < 4; i++)
+        inventory[i] = NULL;
+}
+
+Character::Character(Character const &copy)
+{
+    name = copy.name;
+    for (int i = 0; i < 4; i++)
+    {
+        if (copy.inventory[i])
+            inventory[i] = copy.inventory[i]->clone();
+        else
+            inventory[i] = NULL;
+    }
+}
+
+Character &Character::operator=(Character const &copy)
+{
+    if (this != &copy)
+    {
+        name = copy.name;
+        for (int i = 0; i < 4; i++)
+        {
+            if (inventory[i])
+                delete inventory[i];
+            if (copy.inventory[i])
+                inventory[i] = copy.inventory[i]->clone();
+            else
+                inventory[i] = NULL;
+        }
+    }
+    return (*this);
+}
+
+Character::~Character(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (inventory[i])
+            delete inventory[i];
+    }
+}
+
+std::string const &Character::getName(void) const
+{
+    return (name);
+}
+
+void Character::equip(AMateria *m)
+{
+    if(!m)
+        return;
+    for (int i = 0; i < 4; i++)
+    {
+        if (!inventory[i])
+        {
+            inventory[i] = m;
+            break;
+        }
+    }
+}
+
+void Character::unequip(int idx)
+{
+    if (idx >= 0 && idx < 4)
+        inventory[idx] = NULL;
+}
+
+void Character::use(int idx, ICharacter &target)
+{
+    if (idx >= 0 && idx < 4 && inventory[idx])
+        inventory[idx]->use(target);
+}
---
+++ ex03/Character.hpp
@@ -0,0 +1,24 @@
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+
+#include "ICharacter.hpp"
+#include "AMateria.hpp"
+
+class Character : public ICharacter
+{
+    private:
+        std::string name;
+        AMateria* inventory[4];
+
+    public:
+        Character(void);
+        Character(std::string &name);
+        Character(Character const &copy);
+        Character& operator=(Character const &copy);
+        ~Character(void);
+
+        std::string const &getName(void) const;
+        void equip(AMateria* m);
+        void unequip(int idx);
+        void use(int idx, ICharacter& target);
+}
